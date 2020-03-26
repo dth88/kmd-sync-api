6593e96251e86e7230a03d756da0ca766393deb2
root <gdath100500@gmail.com>
2020-03-26 08:22:27 +0100

    add mkdir logs to setup script

--- sync_api_setup.sh
+++ sync_api_setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 sudo apt-get -y update
-sudo apt-get -y install python3.6 python3-pip libgnutls28-dev libcurl4-openssl-dev libssl-dev unzip python3-dev build-essential jq
-pip3 install setuptools
+sudo apt-get -y install python3.6 python3-pip
 
 git clone https://github.com/dathbezumniy/kmd-sync-api.git
 pip3 install -r kmd-sync-api/requirements.txt
+mkdir .komodo
+mkdir kmd-sync-api/logs
+supervisord -c kmd-sync-api/supervisord.conf
+
+
 
 
 
