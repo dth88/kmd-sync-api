022793d93522a502388c9465d1808bf068c9ba4e
root <gdath100500@gmail.com>
2020-03-26 08:42:39 +0100

    added fetchparams to setup script

--- sync_api_setup.sh
+++ sync_api_setup.sh
@@ -5,8 +5,16 @@ sudo apt-get -y install python3.6 python3-pip
 
 git clone https://github.com/dathbezumniy/kmd-sync-api.git
 pip3 install -r kmd-sync-api/requirements.txt
+
 mkdir .komodo
+touch .komodo/komodo.conf
 mkdir kmd-sync-api/logs
+
+wget https://raw.githubusercontent.com/KomodoPlatform/komodo/master/zcutil/fetch-params.sh
+chmod u+x fetch-params.sh
+./fetch-params.sh
+
+
 supervisord -c kmd-sync-api/supervisord.conf
 
 
