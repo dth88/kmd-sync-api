c1ad5b907396924600c234bd0c11d67560e664a6
root <gdath100500@gmail.com>
2020-03-27 08:46:58 +0100

    setuptools fix

--- sync_api_setup.sh
+++ sync_api_setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 sudo apt-get -y update
-sudo apt-get -y install python3.6 python3-pip git
-pip3 install setuptools
+sudo apt-get -y install python3.6 python3-pip git python-setuptools
 
 git clone https://github.com/dathbezumniy/kmd-sync-api.git
 pip3 install -r kmd-sync-api/requirements.txt
