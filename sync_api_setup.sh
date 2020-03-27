e07b5d61fe5c6cff047718b28e57c41ef24a480c
root <gdath100500@gmail.com>
2020-03-27 10:58:25 +0100

    fixed pycurl, uvloop

--- sync_api_setup.sh
+++ sync_api_setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 sudo apt-get -y update
-sudo apt-get -y install python3.6 python3-pip git libcurl4-openssl-dev libcurl4-gnutls-dev libssl-dev libgnutls28-dev python3-dev
-sudo apt-get -y install build-essential pkg-config libc6-dev m4 g++-multilib autoconf libtool ncurses-dev unzip python python-zmq zlib1g-dev wget curl bsdmainutils automake cmake clang
-sudo apt-get -y install python-pycurl
-pip3 install setuptools
-pip3 install wheel slick-bitcoinrpc
+sudo apt-get -y install python3.6 python3-pip git libcurl4-openssl-dev libcurl4-gnutls-dev libgnutls28-dev
+sudo apt-get -y install python3-dev
+sudo apt-get -y install build-essential
+
+pip3 install setuptools wheel
+pip3 install slick-bitcoinrpc
 
 git clone https://github.com/dathbezumniy/kmd-sync-api.git
 pip3 install -r kmd-sync-api/requirements.txt
