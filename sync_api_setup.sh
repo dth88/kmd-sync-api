dd7af3f32f3b6146ada5ffbba2856b0452f4ed7a
root <gdath100500@gmail.com>
2020-03-27 09:29:16 +0100

    pycurl fix, hopefully...

--- sync_api_setup.sh
+++ sync_api_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get -y update
-sudo apt-get -y install python3.6 python3-pip git libcurl4-openssl-dev libssl-dev
+sudo apt-get -y install python3.6 python3-pip git libcurl4-openssl-dev libcurl4-gnutls-dev libssl-dev libgnutls28-dev 
 pip3 install setuptools 
 pip3 install wheel slick-bitcoinrpc
 
